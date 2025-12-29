f1b1b06cb0e3d3311f4b47634eca40b7a39b8104
ming <hongming_1212@hotmail.com>
2025-12-30 01:03:58 +0800

    style: added multi-layer border gradient button as reference

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,11 @@ function App() {
           1
         </div>
 
-        <div className="size-8 bg-white/16 rounded-[10px]" />
+        <div className="size-8 bg-white/16 rounded-[10px] shadow-[0_0_0_2px_#fff,0_0_0_4px_#fb64b6]" />
+
+        <div className="size-8 bg-white/16 rounded-[10px] outline-[#fb64b6] outline-4 ring-2 ring-offset-4 ring-[--linear-gradient-red-blue]" />
+
+        <div className="silver-corner-btn" />
 
         <button
           className="size-8 rounded-[10px] shadow-[2.81px_2.81px_1.58px_-4px_#FFFFFF_inset,-2.11px_-2.11px_0.6px_-2.46px_#FFFFFF_inset]"
@@ -35,6 +39,8 @@ function App() {
           2
         </button>
       </div>
+
+      <div className="multi-blue-layer-btn" />
 
       <div className="gradient-border-container flex-col mt-4 bg-[#1A1E24] px-2.5 py-4 rounded-[10px] drop-shadow-[0px_3px_5px_0px_#00000066] shadow-[0px_1px_1px_0px_#FFFFFF4D_inset] flex items-center justify-center">
         <div className="grid grid-cols-5 w-full p-2 gap-1.5 bg-linear-to-b rounded-lg from-[rgba(33,32,32,0.6)] to-[rgba(22,22,22,0.6)]">
